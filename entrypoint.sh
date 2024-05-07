7c8d32dc3d638e1770812f43a4f56b64820a26fc
nzws <i@yuzu.sh>
2024-05-07 10:29:42 +0900

    Skip permission updates

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,20 +3,20 @@ set -e
 
 create_pid_dir() {
   mkdir -p /run/apt-cacher-ng
-  chmod -R 0755 /run/apt-cacher-ng
-  chown ${APT_CACHER_NG_USER}:${APT_CACHER_NG_USER} /run/apt-cacher-ng
+  # chmod -R 0755 /run/apt-cacher-ng
+  # chown ${APT_CACHER_NG_USER}:${APT_CACHER_NG_USER} /run/apt-cacher-ng
 }
 
 create_cache_dir() {
   mkdir -p ${APT_CACHER_NG_CACHE_DIR}
-  chmod -R 0755 ${APT_CACHER_NG_CACHE_DIR}
-  chown -R ${APT_CACHER_NG_USER}:root ${APT_CACHER_NG_CACHE_DIR}
+  # chmod -R 0755 ${APT_CACHER_NG_CACHE_DIR}
+  # chown -R ${APT_CACHER_NG_USER}:root ${APT_CACHER_NG_CACHE_DIR}
 }
 
 create_log_dir() {
   mkdir -p ${APT_CACHER_NG_LOG_DIR}
-  chmod -R 0755 ${APT_CACHER_NG_LOG_DIR}
-  chown -R ${APT_CACHER_NG_USER}:${APT_CACHER_NG_USER} ${APT_CACHER_NG_LOG_DIR}
+  # chmod -R 0755 ${APT_CACHER_NG_LOG_DIR}
+  # chown -R ${APT_CACHER_NG_USER}:${APT_CACHER_NG_USER} ${APT_CACHER_NG_LOG_DIR}
 }
 
 create_pid_dir
